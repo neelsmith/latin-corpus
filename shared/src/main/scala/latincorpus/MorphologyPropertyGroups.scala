19f398c216191cdb48f60e3c79fd36ef05c1c0eb
Neel Smith <nsmith@Technites.local>
2020-07-24 07:11:44 -0400

    Mv to jvm project

--- shared/src/main/scala/latincorpus/MorphologyPropertyGroups.scala
+++
@@ -1,8 +0,0 @@
-package edu.holycross.shot.latincorpus
-
-import edu.holycross.shot.tabulae._
-
-
-case class PersonNumber(person: Person, grammaticalNumber: GrammaticalNumber)
-
-case class TenseMood(tense: Tense, mood: Mood)
