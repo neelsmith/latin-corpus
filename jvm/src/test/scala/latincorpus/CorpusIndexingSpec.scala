32dc5471a1cc3a3c84c27dc38b25a32b70b2ae19
Neel Smith <nsmith@Technites.local>
2020-08-15 17:15:41 -0400

    ORganize unit tests better

--- jvm/src/test/scala/latincorpus/CorpusIndexingSpec.scala
+++ jvm/src/test/scala/latincorpus/CorpusIndexingSpec.scala
@@ -1,65 +1,36 @@
 
 package edu.holycross.shot.latincorpus
 
-import edu.holycross.shot.cite._
-import edu.holycross.shot.ohco2._
-import edu.holycross.shot.tabulae._
-import edu.holycross.shot.mid.orthography._
-import edu.holycross.shot.latin._
-import edu.holycross.shot.tabulae._
-import edu.holycross.shot.histoutils._
+
+
 import org.scalatest.FlatSpec
-import scala.io._
+
 
 class CorpusIndexingSpec extends FlatSpec {
 
-val fst = """> sed
-<u>livymorph.indecl18</u><u>ls.n43291</u>sed<indecl><indeclconj><div><indeclconj><indecl><u>indeclinfl.2</u>
-> debebatur
-<u>latcommon.verbn12387</u><u>ls.n12387</u><#>deb<verb><conj2><div><conj2><verb>ebatur<3rd><sg><impft><indic><pass><u>latcommon.ere_conj2impft9</u>
-> ut
-<u>livymorph.indecl33</u><u>ls.n49975</u>ut<indecl><indeclconj><div><indeclconj><indecl><u>indeclinfl.2</u>
-> opinor
-<u>latcommon.verbn32747</u><u>ls.n32747</u><#>opin<verb><conj1><div><conj1><verb>or<1st><sg><pres><indic><pass><u>latcommon.are_conj1pres7</u>
-> fatis
-<u>latcommon.n17799</u><u>ls.n17799</u>fat<noun><neut><us_i><div><us_i><noun>is<neut><dat><pl><u>livymorph.us_i20</u>
-<u>latcommon.n17799</u><u>ls.n17799</u>fat<noun><neut><us_i><div><us_i><noun>is<neut><abl><pl><u>livymorph.us_i22</u>
-> tantae
-<u>latcommon.adjn47473</u><u>ls.n47473</u>tant<adj><us_a_um><div><us_a_um><adj>ae<fem><nom><pl><pos><u>latcommoninfl.us_a_um42</u>
-<u>latcommon.adjn47473</u><u>ls.n47473</u>tant<adj><us_a_um><div><us_a_um><adj>ae<fem><gen><sg><pos><u>latcommoninfl.us_a_um38</u>
-<u>latcommon.adjn47473</u><u>ls.n47473</u>tant<adj><us_a_um><div><us_a_um><adj>ae<fem><dat><sg><pos><u>latcommoninfl.us_a_um39</u>
-<u>latcommon.adjn47473</u><u>ls.n47473</u>tant<adj><us_a_um><div><us_a_um><adj>ae<fem><voc><pl><pos><u>latcommoninfl.us_a_um48</u>
-<u>livymorph.adj30</u><u>ls.n47473</u>tant<adj><us_a_um><div><us_a_um><adj>ae<fem><nom><pl><pos><u>latcommoninfl.us_a_um42</u>
-<u>livymorph.adj30</u><u>ls.n47473</u>tant<adj><us_a_um><div><us_a_um><adj>ae<fem><gen><sg><pos><u>latcommoninfl.us_a_um38</u>
-<u>livymorph.adj30</u><u>ls.n47473</u>tant<adj><us_a_um><div><us_a_um><adj>ae<fem><dat><sg><pos><u>latcommoninfl.us_a_um39</u>
-<u>livymorph.adj30</u><u>ls.n47473</u>tant<adj><us_a_um><div><us_a_um><adj>ae<fem><voc><pl><pos><u>latcommoninfl.us_a_um48</u>
-> origo
-<u>latcommon.n32994</u><u>ls.n32994</u>orig<noun><fem><o_inis><div><o_inis><noun>o<fem><nom><sg><u>latcommoninfl.o_inis13</u>
-<u>latcommon.n32994</u><u>ls.n32994</u>orig<noun><fem><o_inis><div><o_inis><noun>o<fem><voc><sg><u>latcommoninfl.o_inis23</u>
-> urbis
-<u>latcommon.n49895</u><u>ls.n49895</u>urb<noun><fem><s_is><div><s_is><noun>is<fem><acc><pl><u>latcommoninfl.s_is21b</u>
-<u>latcommon.n49895</u><u>ls.n49895</u>urb<noun><fem><s_is><div><s_is><noun>is<fem><gen><sg><u>latcommoninfl.s_is14</u>
-""".split("\n").toVector
-
-
-
-  val cn1 = CitableNode(
-    CtsUrn("urn:cts:omar:stoa0179.stoa001.omar:1.4.1"),
-    "sed debebatur, ut opinor, fatis tantae origo urbis"
-  )
-  val corpus = Corpus(Vector(cn1))
-  val ortho = Latin24Alphabet
-  val lc = LatinCorpus.fromFstLines(corpus,ortho,fst)
+  val corpusFile = "jvm/src/test/resources/hyginus-latc.cex"
+  val chapterFile = "jvm/src/test/resources/c108a.cex"
+  val corpus = LatinCorpus.fromFile(corpusFile)
+  val chapter = LatinCorpus.fromFile(chapterFile)
 
   "A LatinCorpus" should "index from tokens to lexemes" in {
-    val expectedNumberLexemes = 1
-    val expectedLexVector = Vector("ls.n32747")
-    assert(lc.tokenLexemeIndex("opinor").size == expectedNumberLexemes)
-    assert(lc.tokenLexemeIndex("opinor") == expectedLexVector)
+    val expectedSize = chapter.analyzed.map(_.text).distinct.size
+
+    assert(chapter.tokenLexemeIndex.size == expectedSize)
+    // Test relative/interrogative ambiguity:
+    val expectedLexemes = 2
+    assert(chapter.tokenLexemeIndex("quem").size == expectedLexemes)
   }
-  it should "index from lexemes to tokens" in pending /*{
-    val expectedVector = Vector("ls.n43291")
-    assert(lc.lexemeTokenIndex("sed") == expectedVector)
+
+  it should "index from lexemes to tokens" in {
+    val expectedForms = Set("sunt", "essent", "est")
+    val sum = "ls.n46529"
+    assert(chapter.lexemeTokenIndex(sum).toSet == expectedForms)
+  }
+
+  it should "pair lexemes and tokens" in pending /*{
+    // This is OK: figure out a good test for it:
+    println(chapter.lexemeTokenPairings.size)
   }*/
 
 }
--- jvm/src/test/scala/latincorpus/CorpusNavigationSpec.scala
+++ jvm/src/test/scala/latincorpus/CorpusNavigationSpec.scala
@@ -52,25 +52,6 @@ class CorpusNavigationSpec extends FlatSpec {
   }
 
 
-  it should "index tokens to lexemes" in {
-    val expectedSize = chapter.analyzed.map(_.text).distinct.size
-
-    assert(chapter.tokenLexemeIndex.size == expectedSize)
-    // Test relative/interrogative ambiguity:
-    val expectedLexemes = 2
-    assert(chapter.tokenLexemeIndex("quem").size == expectedLexemes)
-  }
-  it should "index lexemes to tokens" in {
-    val expectedForms = Set("sunt", "essent", "est")
-    val sum = "ls.n46529"
-    assert(chapter.lexemeTokenIndex(sum).toSet == expectedForms)
-  }
-
-  it should "pair lexemes and tokens" in pending /*{
-    // This is OK: figure out a good test for it:
-    println(chapter.lexemeTokenPairings.size)
-  }*/
-
 
 
 }
