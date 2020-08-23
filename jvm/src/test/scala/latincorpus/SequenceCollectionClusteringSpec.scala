f8c10b8feff03d32827192aedcb39f706e1d74c0
Neel Smith <nsmith@Technites.local>
2020-08-23 15:25:21 -0400

    Spec out more tests

--- jvm/src/test/scala/latincorpus/SequenceCollectionClusteringSpec.scala
+++ jvm/src/test/scala/latincorpus/SequenceCollectionClusteringSpec.scala
@@ -9,5 +9,4 @@ class SequenceCollectionClusteringSpec extends FlatSpec {
   "A ParsedSequenceCollection" should "recluster all tokens as LatinEditions" in pending
   it should "recluster all tokens as citableUnits"  in pending
   it should "recluster all tokens as ngrams" in pending
-
 }
